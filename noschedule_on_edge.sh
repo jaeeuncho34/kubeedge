67a8ebecda0ba5cb2c780ac855426f5702759deb
jaeeuncho34 <60213218+jaeeuncho34@users.noreply.github.com>
2021-11-11 22:20:47 +0900

    Create noschedule_on_edge.sh

---
+++ noschedule_on_edge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NodeSelectorPatchJson='{"spec":{"template":{"spec":{"nodeSelector":{"node-role.kubernetes.io/master": "","node-role.kubernetes.io/worker": ""}}}}}'
+
+NoShedulePatchJson='{"spec":{"template":{"spec":{"affinity":{"nodeAffinity":{"requiredDuringSchedulingIgnoredDuringExecution":{"nodeSelectorTerms":[{"matchExpressions":[{"key":"node-role.kubernetes.io/edge","operator":"DoesNotExist"}]}]}}}}}}}'
+
+for((i=2;i<6;i++));
+do
+        edgenode="kubesphere-$i"
+
+        namespaces=($(kubectl get pods -A -o wide |egrep -i $edgenode | awk '{print $1}' ))
+        pods=($(kubectl get pods -A -o wide |egrep -i $edgenode | awk '{print $2}' ))
+        length=${#namespaces[@]}
+
+
+        for((i=0;i<$length;i++));
+        do
+                ns=${namespaces[$i]}
+                pod=${pods[$i]}
+                resources=$(kubectl -n $ns describe pod $pod | grep "Controlled By" |awk '{print $3}')
+                echo "Patching for ns:"${namespaces[$i]}",resources:"$resources
+                kubectl -n $ns patch $resources --type merge --patch "$NoShedulePatchJson"
+                sleep 1
+        done
+done
